56176a07a0838f3c2b04bcf90b3b02f501290a3b
KnightryryV2 <116779059+KnightryryV2@users.noreply.github.com>
2022-11-17 17:31:29 +0000

    make calculators

---
+++ python things/calc.py
@@ -0,0 +1,17 @@
+num1 = float(input("What is the first number: "))
+num2 = float(input("What is the second number: "))
+oper = input("What Operation: ")
+print("The soloution is: ")
+if(oper == "/"):
+    print(num1 / num2)
+
+elif(oper == "*"): 
+    print(num1 * num2)
+
+elif(oper == "+"):
+    print(num1 + num2)
+
+elif(oper == "-"):
+    print(num1 - num2)
+
+
