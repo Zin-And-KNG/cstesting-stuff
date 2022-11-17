6763487584d9ab4b9a7c1d377af48c73437300e4
KnightryryV2 <116779059+KnightryryV2@users.noreply.github.com>
2022-11-17 08:11:09 -0500

    more test

--- testing.cs
+++ testing.cs
@@ -4,6 +4,15 @@ namespace testing
 {
     public class testing
     {
-        Console.WriteLine("Test");
+        public void Main (string[] args) 
+        {
+            Console.WriteLine("Help me");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What Operation?")
+            str operation = Console.ReadLine();
+            
+        }
+        
     }
 }
