142f9655ab04eb49b9a4f3836e310b97ebae1736
KnightryryV2 <116779059+KnightryryV2@users.noreply.github.com>
2022-12-07 18:41:24 +0000

    yes

--- python things/calc.py
+++ python things/calc.py
@@ -1,7 +1,7 @@
 num1 = float(input("What is the first number: "))
 num2 = float(input("What is the second number: "))
 oper = input("What Operation: ")
-print("The soloution is: ")
+print("The soloution is: ", end=" ")
 if(oper == "/"):
     print(num1 / num2)
 
